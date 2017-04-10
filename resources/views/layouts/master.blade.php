b2fa0c87c8230eb649835d18ab7b6c9c581d0b17
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-10 14:32:22 +0700

    Try to fix issue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,12 +12,12 @@
 
     <!-- Bootstrap Core CSS -->
 
-    <link href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css',true) }}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="{{ asset('/vendor/font-awesome/css/font-awesome.min.css') }}"
+    <link href="{{ asset('/vendor/font-awesome/css/font-awesome.min.css',true) }}"
     rel="stylesheet" type="text/css">
-    <link href="{{ asset('/css/kursusin.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('/css/kursusin.css',true) }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">    
         @include('layouts.nav')
@@ -27,7 +27,7 @@
         @include('layouts.footer')
 
     <!-- jQuery -->
-    <script src="{{ asset('/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('/vendor/jquery/jquery.min.js',true)}}"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="/public/vendor/bootstrap/js/bootstrap.min.js"></script>
