1af5915ec90a3aac822b425833f7ac6785fc5626
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-10 14:09:38 +0700

    Test

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -48,12 +48,3 @@
             </div>
         </div>
     </footer>
-
-    <!-- jQuery -->
-    <script src="{{ URL::asset('vendor/jquery/jquery.min.js' )}} "></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,15 +8,15 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Freelancer - Start Bootstrap Theme</title>
+    <title>Kursusin</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{ URL::asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="/public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="{{ URL::asset('vendor/font-awesome/css/font-awesome.min.css') }}"
+    <link href="/public/vendor/font-awesome/css/font-awesome.min.css"
     rel="stylesheet" type="text/css">
-    <link href="{{ URL::asset('css/kursusin.css') }}" rel="stylesheet" type="text/css">
+    <link href="/public/css/kursusin.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">    
         @include('layouts.header')
@@ -24,6 +24,15 @@
         @yield('content')
     </div>
         @include('layouts.footer')
+
+    <!-- jQuery -->
+    <script src="/public/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/public/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 </body>
 
