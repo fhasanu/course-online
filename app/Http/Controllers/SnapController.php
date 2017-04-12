cf1e9deac3c2f65b8791645e5a00adcc9e8f165e
carash <wibisanawidirga@gmail.com>
2017-04-12 13:08:20 +0900

    snaptoken fix 8

--- app/Http/Controllers/SnapController.php
+++ app/Http/Controllers/SnapController.php
@@ -24,8 +24,6 @@ class SnapController extends Controller
     {
         error_log('masuk ke snap token dari ajax');
 
-        return 'TOKEN HERE';
-
         $midtrans = new Midtrans();
         Midtrans::config([
             'server_key' => 'VT-server-USJezPOK2HxDwG-kvkrDDugI',
