284d511c31b05c28e9898a3b7ffde7e1c12a4fb8
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-15 13:49:05 +0700

    I want to nap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,7 @@
 <?php
 $url = parse_url(getenv("postgres://neckwlogcyduqj:4567149203fced87aacf01dd0e2552e8be3a0c0bd927f77e28165853802d5371@ec2-54-235-153-124.compute-1.amazonaws.com:5432/daju7u2osc727b"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-echo($host);
-echo($username);
-echo($password);
-echo($database);
+echo($url);
 ?>
 	Testdrive
 	HelloHai
