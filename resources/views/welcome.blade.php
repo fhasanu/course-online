4bce1a0175bea4ed30de7e61d2062c3a38e1689a
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-15 13:50:40 +0700

    I want to nap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <?php
 $url = parse_url(getenv("postgres://neckwlogcyduqj:4567149203fced87aacf01dd0e2552e8be3a0c0bd927f77e28165853802d5371@ec2-54-235-153-124.compute-1.amazonaws.com:5432/daju7u2osc727b"));
 
-echo($url);
+var_dump($url);
 ?>
 	Testdrive
 	HelloHai
