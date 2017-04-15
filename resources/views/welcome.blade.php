d105435883b02d76eb79e3b828b089ff29912aaf
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-15 13:46:27 +0700

    I want to nap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
+<?php
+$url = parse_url(getenv("postgres://neckwlogcyduqj:4567149203fced87aacf01dd0e2552e8be3a0c0bd927f77e28165853802d5371@ec2-54-235-153-124.compute-1.amazonaws.com:5432/daju7u2osc727b"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+echo($host);
+echo($username);
+echo($password);
+echo($database);
+?>
 	Testdrive
 	HelloHai
 @endsection
