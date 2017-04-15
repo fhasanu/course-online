32f5e09a6864ecfe5f2df02e97e0fd2d556024ff
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-15 13:54:39 +0700

    Hope

--- config/database.php
+++ config/database.php
@@ -1,11 +1,9 @@
 <?php
 
-$url = parse_url(getenv("postgres://neckwlogcyduqj:4567149203fced87aacf01dd0e2552e8be3a0c0bd927f77e28165853802d5371@ec2-54-235-153-124.compute-1.amazonaws.com:5432/daju7u2osc727b"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$username = 'neckwlogcyduqj';
+$password = '4567149203fced87aacf01dd0e2552e8be3a0c0bd927f77e28165853802d5371';
+$host = 'ec2-54-235-153-124.compute-1.amazonaws.com';
+$database = 'daju7u2osc727b';
 
 return [
 
