f8588698a097408e6ec5f8aec2ef57bf24de021c
carash <wibisanawidirga@gmail.com>
2017-04-17 14:02:17 +0900

    finished models

--- app/CourseDetail.php
+++ app/CourseDetail.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourseDetail extends Model
 {
-    //
+    protected $table = 'ak_course_detail';
+    protected $primaryKey = 'ak_course_detail_id';
+
+    protected $fillable = [
+    	'ak_course_id',
+    	'ak_course_detail_name',
+    	'ak_course_detail_price',
+    	'ak_course_detail_level',
+    	'ak_course_detail_age',
+    	'ak_course_detail_size',
+    	'ak_course_detail_desc',
+    ];
+
+    public $timestamps = false;
 }
