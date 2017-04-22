ba8ce018f6afeb55256851cbc97a220085d1904e
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-22 22:56:33 +0700

    A

--- resources/views/snap_checkout.blade.php
+++ resources/views/snap_checkout.blade.php
@@ -10,7 +10,7 @@
             <input type="hidden" name="result_type" id="result-type" value=""></div>
             <input type="hidden" name="result_data" id="result-data" value=""></div>
         </form>
-        {{$cart}}
+        {{var_dump($cart)}}
         @if (empty($cart))
 
         <h1>EMPTY</h1>
