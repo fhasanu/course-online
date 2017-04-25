f4456ab3de21bfbdf652a6435571bb902e1a21a6
Ahmad Elang Islam Belanaby <sayaelang@gmail.com>
2017-04-25 20:32:43 +0700

    Elang Test

--- resources/views/payment_finish.blade.php
+++ resources/views/payment_finish.blade.php
@@ -4,11 +4,11 @@
 <div class="alert alert-success" role="alert">Transaksi Sukses</div>
 <div class="panel panel-default">
 <div class="panel panel-body">
-	<div>Status 	:	{{ucfirst($snap->status_message)}}</div>
+{{-- 	<div>Status 	:	{{ucfirst($snap->status_message)}}</div>
 	<div>Harga		:	Rp. {{$snap->gross_amount}}</div>
 	<div>Pembayaran	:	{{ucfirst(preg_replace('/[^A-Za-z0-9\-]/', ' ', $snap->payment_type))}}</div>	
 	<div>Waktu		:	{{$snap->transaction_time}}</div>	
-</div>
+ --}}</div>
 -->
 <h1>Payment Finish</h1>
 <pre>{{$request}}</pre>
